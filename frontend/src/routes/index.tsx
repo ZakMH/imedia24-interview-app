3f22b8ecb8dbfa38dfc26dc9dda0b45ef7358f9c
M'harek Zakaria <zmharek@sqli.com>
2023-03-09 14:20:39 +0100

    add error page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage, CategoryPage } from "../pages/";
+import ErrorPage from "../pages/ErrorPage";
 import { RootLayout } from "./RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
